acc9e77663aa03752aba55e8bd5e6cd958e232b0
JJSG - UC <juanjose.sepulvedagarcia@pg.canterbury.ac.nz>
2024-11-21 18:08:43 +1300

    Initial changes to all the functions

--- target_fas.py
+++ target_fas.py
@@ -1,24 +1,137 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-def source_spectrum():
+def source_spectrum(freqs, M_w, Vs_src, rho_src, dSigma, R0):
     '''returns source spectrum'''
-    return None
 
-def path_spectrum():
+    def magConv(M_w):
+        M_0 = 10**((3/2)*(M_w + 10.7)) # eqtn 2.5, Baker et al. (2021)
+        return M_0
+
+    def cornerFreq(M_0, Vs_src, dSigma):
+            f0 = 4.9e6*Vs_src*(dSigma/M_0)**(1/3) # eqtn 4
+            return f0
+    
+    def constantC(Vs_src, rho_src, R0):
+        R_TP= 0.55 # radiation pattern (usually 0.55) 
+        V = 1/np.sqrt(2) # page 641, Boore (2003)
+        F = 2 # effect of the free surface, page 641 Boore (2003)
+
+        C = R_TP*V*F/(4*np.pi*rho_src*(Vs_src**3)*R0)
+        C = C*(1/1000**3)*(1/100**3) # To cancel cm3 with km3, final units= s^3/(g*km)
+        return C
+
+    M_0 = magConv(M_w)
+    f0 = cornerFreq(M_0, Vs_src, dSigma) # eqtn 4, Boore (2003)
+    C = constantC(Vs_src, rho_src, R0) # eqtn 7, Boore (2003)
+    
+    # Sa = 1/((1 + (freqs/f0)**2)) # table 2, Boore (2003)
+    Sa = (4*np.pi*freqs**2)/((1 + (freqs/f0)**2)) # table 2, Boore (2003)
+    Sb = 1 # table 2, Boore (2003)
+    S = Sa*Sb # eqtn 6, Boore (2003)
+
+    E = C*M_0*S # eqtn 5, Boore (2003)
+    
+    return E
+
+def path_spectrum(freqs, R, Z_r, Q_f, c_Q):
     '''returns path spectrum'''
-    return None
 
-def site_spectrum():
+    Q_fs = np.array(list(map(Q_f, freqs)))
+    P = Z_r(R)*np.exp(-np.pi*freqs*R/(Q_fs*c_Q)) # eqtn 8, Boore (2003)
+    
+    return P
+
+def site_spectrum(freqs, k0):
     '''return site spectrm'''
-    return None
+    def A_f():
+        return 1
 
-def get_targetFAS():
+    def D_f(freqs, k0):
+        d_f = np.exp(-np.pi*k0*freqs) # eqtn 20, Boore (2003)
+        # d_f = 1/np.sqrt(1+(freqs/15)**8)
+        return d_f
+
+    d_f = D_f(freqs, k0)
+    a_f = A_f()
+
+    G = a_f*d_f
+    
+    return G
+       
+
+def get_targetFAS(freqs, sourceParam, pathParam, siteParam):
     '''returns target Fourier Amplitude Spectrum'''
-    return None
 
-def plot_targetFAS():
+    # Source parameters
+    M_w = sourceParam['M_w']
+    Vs_src = sourceParam['Vs_src']
+    rho_src = sourceParam['rho_src']
+    dSigma = sourceParam['dSigma']
+    R0 = 1 # reference distance, usually taken as 1, page 642 Boore (2003)
+
+    # Path parameters
+    R = pathParam['R']
+    Z_r = pathParam['Z_r']
+    Q_f = pathParam['Q_f'] 
+    c_Q = pathParam['c_Q']
+
+    # Site parameters
+    k0 = siteParam['k0']
+
+
+    # Computation of spectrums
+    E = source_spectrum(freqs, M_w, Vs_src, rho_src, dSigma, R0) # Source spectrum 
+    P = path_spectrum(freqs, R, Z_r, Q_f, c_Q)
+    G = site_spectrum(freqs, k0)
+
+    target_FAS = E*P*G
+
+    return target_FAS
+
+
+def plot_targetFAS(freqs, sourceParam, pathParam, siteParam):
     '''plots target Fourier Amplitude Spectrum'''
+
+    fas = get_targetFAS(freqs, sourceParam, pathParam, siteParam)
+
+    plt.plot(freqs, fas, label='Target FAS')
+    plt.title('Target Fourier Amplitude Spectrum')
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('Fourier Amplitude')
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.legend()
+    plt.grid(which='both', alpha=0.5)
+    plt.show()
+
+
     return None
 
 if __name__=='__main__':
-    print('Nothing to show for the moment')
+
+    freqs = np.linspace(0.1, 100, 5000)
+
+    sourceParam = {'M_w': 4.0, 
+                   'Vs_src': 3.6, # km/s
+                   'rho_src': 2800, # gm/cc
+                   'dSigma':100 # bars
+                   }
+    
+    Z_r = lambda r: 1/r if r<40 else ((1/40)*((40/r)**0.5) if r>=40 else None)
+    Q_f = lambda f: 680*(f**0.38) # page 643, Boore (2003)
+    pathParam = {'R':10, # km
+                 'Z_r': Z_r, # picewise funct
+                 'Q_f': Q_f, # table 4 Boore (2003)
+                 'c_Q': 3.5 # km/s, table 4 Boore (2003)
+                 }
+
+    siteParam = {'k0':0.045 # Lee et al. (2022)
+                 }
+
+
+    # fas = get_targetFAS(freqs, sourceParam, pathParam, siteParam)
+    plot_targetFAS(freqs, sourceParam, pathParam, siteParam)
+
+
+
--- white_noise.py
+++ white_noise.py
@@ -1,12 +1,28 @@
 import numpy as np
+from utils import fft
+import matplotlib.pyplot as plt
 
 def windowB():
     '''Box window function'''
     return None
 
-def windowE():
+def windowE(times, duration): # after Saragoni and Hart (1973)
     '''Exponential window function'''
-    return None
+
+    epsilon = 0.3 # Saragoni and Hart (1973), page 655 Boore (2003)
+    eta = 0.05 # Saragoni and Hart (1973), page 655 Boore (2003)
+
+    # Eqtns 25, 26, and 27, Boore (2003)
+    b = -(epsilon*np.log(eta))/(1+epsilon*(np.log(epsilon) - 1)) 
+    c = b/epsilon
+    a = (np.exp(1)/epsilon)**b
+
+    f_Tgm = 1/1.3 # page 655, Boore (2003) 
+    t_eta = duration*f_Tgm # eqtn 28, Boore (2003)
+    
+    w = a*((times/t_eta)**b)*np.exp(-c*(times/t_eta)) # eqn 24, Boore (2003)
+
+    return w, t_eta
 
 def get_noiseB():
     '''white noise with a box window function'''
@@ -14,10 +30,79 @@ def get_noiseB():
 
 def get_noiseE():
     '''white noise with an Exponential window function'''
-    return None
 
-def plot_noise():
+    gmDuration = gmParam['gmDuration']
+    sampleRate = gmParam['sampleRate']
+
+    times = np.linspace(0, gmDuration, sampleRate*gmDuration)
+
+    noise0 = np.random.normal(0, 1, sampleRate*gmDuration) # gaussian noise
+    window, t_eta = windowE(times, gmDuration)
+
+    windowed_noise = noise0*window
+    fas_wn, ff_wn = fft(windowed_noise, 1/sampleRate, windowed_noise.shape[0])
+    fas_phase = np.angle(fas_wn)
+    fas_wn = np.abs(fas_wn)
+    root_mean_square = np.sqrt(np.mean(fas_wn**2))
+    normalized_spectral_amplitudes = fas_wn/root_mean_square
+
+    return normalized_spectral_amplitudes, fas_phase, ff_wn
+
+def plot_noise(gmParam):
+    '''plots noise given input paramters'''
+
+    gmDuration = gmParam['gmDuration']
+    sampleRate = gmParam['sampleRate']
+
+    times = np.linspace(0, gmDuration, sampleRate*gmDuration)
+
+    noise0 = np.random.normal(0, 1, sampleRate*gmDuration) # gaussian noise
+    window, t_eta = windowE(times, gmDuration)
+
+    windowed_noise = noise0*window
+    fas_wn, ff_wn = fft(windowed_noise, 1/sampleRate, windowed_noise.shape[0])
+    fas_phase = np.angle(fas_wn)
+    fas_wn = np.abs(fas_wn)
+    root_mean_square = np.sqrt(np.mean(fas_wn**2))
+    normalized_spectral_amplitudes = fas_wn/root_mean_square
+
+    fig, axs = plt.subplots(2,2, figsize=(10, 8))
+    fig.suptitle('White Noise for SMSIM')
+    axs[0, 0].plot(times, noise0, linewidth=0.35, 
+                   label='Gaussian WN')
+    axs[0, 1].plot(times, windowed_noise, linewidth=0.35, 
+                   label='Windowed WN')
+    axs[0, 1].plot(times, window, linewidth=1.25, 
+                   label='Window function')
+    axs[1, 0].plot(ff_wn, fas_wn, linewidth=0.35, 
+                   label='Windowed WN')
+    axs[1, 1].plot(ff_wn, normalized_spectral_amplitudes, linewidth=0.35, 
+                   label='Normalized windowed WN')
+
+    for i in [0, 1]:
+        axs[0, i].set_xlabel('Time (s)')
+        axs[0, i].set_ylabel('Acceleration (cm/s2)')
+        axs[0, i].set_xlim(0, gmDuration)
+        axs[0, i].legend()
+        
+    for i in [0, 1]:
+        axs[1, i].set_xlabel('Frequency (HZ)')
+        axs[1, i].set_ylabel('Fourier Amplitude')
+        axs[1, i].set_xscale('log')
+        axs[1, i].set_yscale('log')
+        axs[1, i].legend()
+
+
+
+    plt.show()
+
     return None
 
 if __name__=='__main__':
-    print('Nothing to show for the moment')
+
+    gmParam = {'gmDuration': 25, # sec
+               'sampleRate':200 # Hz
+              }
+    
+    # noise_spectra, phase, ffreqs = noise(gmParam)
+    plot_noise(gmParam)
